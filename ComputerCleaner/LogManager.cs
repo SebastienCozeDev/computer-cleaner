30fb58fa52aec305a3a645795ea5529d86faf73f
Sébastien <sebastien.coze.62@outlook.fr>
2022-12-03 16:26:33 +0100

    Ajout du système de log.

---
+++ ComputerCleaner/LogManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ComputerCleaner
+{
+    /// <summary>
+    /// Classe gérant les logs.
+    /// </summary>
+    public class LogManager
+    {
+        /// <summary>
+        /// Chemin du fichier de logs principal.
+        /// </summary>
+        private const string LOG_FILE = "cleaner.log";
+
+        /// <summary>
+        /// Ajoute une ligne dans le fichier de logs.
+        /// </summary>
+        /// <param name="type">Type du log.</param>
+        /// <param name="log">Content du log.</param>
+        public static void AddInLog(string type, string log)
+        {
+            string[] content;
+            try
+            {
+                content = new string[File.ReadAllLines(LOG_FILE).Length + 1];
+                content = File.ReadAllLines(LOG_FILE);
+                foreach (string line in content)
+                {
+                    Console.WriteLine(line);
+                }
+                content[content.Length - 1] = "[" + type.ToUpper() + "] " + log;
+                File.WriteAllLines(LOG_FILE, content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                File.WriteAllText(LOG_FILE, "[" + type.ToUpper() + "] " + log);
+            }
+        }
+    }
+}
--- ComputerCleaner/MainWindow.xaml.cs
+++ ComputerCleaner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ComputerCleaner
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Classe d'interaction logique pour <c>MainWindow.xaml</c>.
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Crée une instance de <c>MainWindow</c>.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void UpdateButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Votre version du logiciel est à jour.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            LogManager.AddInLog("Information", "Your software version is up to date.");
+        }
+
+        private void CleanButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cette fonctionnalité n'est pas disponible pour cette version du logiciel.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            LogManager.AddInLog("Information", "This feature is not available for this version of the software.");
+        }
+
+        private void HistoryButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cette fonctionnalité n'est pas disponible pour cette version du logiciel.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            LogManager.AddInLog("Information", "This feature is not available for this version of the software.");
+        }
+
+        private void WebsiteButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://sebastiencozedev.github.io/e-portfolio/")
+                {
+                    UseShellExecute = true,
+                });
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                LogManager.AddInLog("Error", ex.Message);
+            }
         }
 
     }
